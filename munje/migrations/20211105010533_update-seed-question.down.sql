c89456b415d97c9133b7dde9d97f5aaf1bcf68eb
Eric Walker <eric.walker@gmail.com>
2021-11-04 19:14:34 -0600

    Update the details of the seed question

---
+++ munje/migrations/20211105010533_update-seed-question.down.sql
@@ -0,0 +1,8 @@
+update questions
+  set
+    external_id = 'ya2',
+    title = 'How do I dump the data of some SQLite3 tables?',
+    text = 'Visit [this site](https://stackoverflow.com/questions/75675/how-do-i-dump-the-data-of-some-sqlite3-tables) and see how you do.',
+    link = 'https://stackoverflow.com/questions/75675/how-do-i-dump-the-data-of-some-sqlite3-tables',
+    link_logo = 'https://cdn.sstatic.net/Sites/stackoverflow/Img/apple-touch-icon.png?v=c78bd457575a'
+  where id = 1;
---
+++ munje/migrations/20211105010533_update-seed-question.up.sql
@@ -0,0 +1,8 @@
+update questions
+  set
+    external_id = 'LD',
+    title = '4. Median of Two Sorted Arrays',
+    text = 'Complete the challenge at [this link](https://leetcode.com/problems/median-of-two-sorted-arrays/). When you''re done, come back to this question and indicate whether you solved the problem.',
+    link = 'https://leetcode.com/problems/median-of-two-sorted-arrays/',
+    link_logo = 'https://assets.leetcode.com/static_assets/public/icons/favicon-96x96.png'
+  where id = 1;
